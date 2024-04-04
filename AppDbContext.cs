860a647c5e0964446d875af728dd953a0998e9fb
Name1 <nchernyaev06@gmail.com>
2024-04-04 11:38:39 +0400

    day3

---
+++ AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿namespace WpfApp5Ларионова1
+{
+    using Microsoft.EntityFrameworkCore;
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+    }
+}
---
+++ Window1.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp5Ларионова1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void switch_Click(object sender, RoutedEventArgs e)
+        {
+            var login = LoginBox.Text;
+            var password = PasswordBox.Text;
+
+            var context = new AppDbContext();
+
+            var user = context.Users.SingleOrDefault(x => x.Login == login && x.Password == password);
+            if (user == null)
+            {
+                MessageBox.Show("Такой пользователь уже в клубе крутяшек!");
+                return;
+            }
+            MessageBox.Show("Вы успешно вошли в аккаунт!");
+
+        }
+        private void login_click(object sender, RoutedEventArgs e)
+        {
+            var login = LoginBox.Text;
+            var password = PasswordBox.Text;
+            var context = new AppDbContext();
+            var user = context.Users.SingleOrDefault(x => x.Login == login && x.Password == password);
+
+            if (user == null)
+            {
+                result_text.Text = "Такой пользователь уже в клубе крутяшек!";
+                return;
+            }
+            var Login = new User { Login = login, Password = password };
+            context.Users.Add(Login);
+            context.SaveChanges();
+            MessageBox.Show("Welcome to the club");
+        }
+    }
+}
